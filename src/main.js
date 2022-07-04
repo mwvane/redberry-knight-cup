4e95610b1e3f28c55c4857cfc17163558a004411
leomw <bzishvili57@gmail.com>
2022-07-04 12:20:03 +0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {
+    faCircleRight,
+}
+    from "@fortawesome/free-regular-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
+library.add(
+    faCircleRight,
+)
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+    .use(store)
+    .use(router)
+    .use(BootstrapVue3)
+    .component("font-awesome-icon",FontAwesomeIcon)
+
+app.mount('#app')
---
+++ src/views/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-6">
+        <div class="w-100 p-2" style="background-color: #7025FB">
+          <p>Redberry knight cup</p>
+        </div>
+        <h1>image here</h1>
+      </div>
+      <div class="col-6">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
