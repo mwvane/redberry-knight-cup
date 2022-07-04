8917f6d3e95bc8c39583622b6b0f4707b8d46788
leomw <bzishvili57@gmail.com>
2022-07-04 20:12:45 +0400

    Implement "update" function to save data in localStorage

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-container">
-    <div class="row">
+    <div class="row" v-if="level === 'Personal info'">
       <div class="col-6">
         <div class="w-100 p-2" style="background-color: #7025FB;">
           <p class="my-2">chess</p>
@@ -17,10 +17,30 @@
         </div>
         <h3 class="my-5" style="color: black">Personal Information</h3>
         <b-form class="w-75 my-2">
-          <b-form-input type="text" class="my-3" v-model="name"></b-form-input>
-          <b-form-input type="email" class="my-3" v-model="email"></b-form-input>
-          <b-form-input type="number" class="my-3" v-model="phone"></b-form-input>
-          <b-form-input type="date" class="my-3" v-model="DOB"></b-form-input>
+          <b-form-input
+              type="text"
+              class="my-3"
+              v-model="model.name"
+              @input="update"
+          ></b-form-input>
+          <b-form-input
+              type="email"
+              class="my-3"
+              v-model="model.email"
+              @input="update"
+          ></b-form-input>
+          <b-form-input
+              type="number"
+              class="my-3"
+              v-model="model.phone"
+              @input="update"
+          ></b-form-input>
+          <b-form-input
+              type="date"
+              class="my-3"
+              v-model="model.date_of_birth"
+              @input="update"
+          ></b-form-input>
           <div class="d-flex justify-content-between py-5">
             <b-button @click="cancel" variant="outline-dark"> Cancel</b-button>
             <b-button @click="submit" variant="primary">Next</b-button>
@@ -28,36 +48,116 @@
         </b-form>
       </div>
     </div>
+
+    <div class="row" v-if="level === 'Experience'">
+      <div class="col-6">
+        <div class="w-100 p-2" style="background-color: #7025FB;">
+          <p class="my-2">chess</p>
+        </div>
+        <img src="../assets/chess3.png" alt="no image" class="w-100">
+        <div class="caption">
+          <p>"MANY HAVE BECOME CHESS MASTER; NO ONE HAS BECOME THE MASTER OF CHESS"</p>
+          <p STYLE="color: white">-SIEGBERT TARRASCH</p>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="w-100 p-2" style="border-bottom: 1px solid rgba(100,100,100, .2)">
+          <p class="my-2" style="color: black">First Step Is Done, Continue To Finish Onboarding</p>
+        </div>
+        <h3 class="my-5" style="color: black">Chess experience</h3>
+        <b-form class="w-75 my-2">
+          <b-form-select class="w-50" v-model="model.experience_level" @input="update">
+            <b-form-select-option v-for="item in experiences" :key="item" :value="item">
+              {{ item }}
+            </b-form-select-option>
+          </b-form-select>
+          <b-form-select class="w-50" v-model="model.character_id" @input="update">
+            <b-form-select-option v-for="person in characters" :key="person.id" :value="person.id">
+              {{ person.name }}
+            </b-form-select-option>
+
+          </b-form-select>
+          <div class="my-5">
+            <p>Have you participated in the Redberry Championship?</p>
+            <b-form-radio name="participated" v-model="model.already_participated" value="true">Yes</b-form-radio>
+            <b-form-radio name="participated" v-model="model.already_participated" value="false">No</b-form-radio>
+          </div>
+
+          <div class="d-flex justify-content-between py-5">
+            <b-button @click="backLevel" variant="outline-dark"> Back</b-button>
+            <b-button @click="done" variant="primary">Done</b-button>
+          </div>
+        </b-form>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: "Register",
-  data(){
-    return{
-      name: '',
-      email: '',
-      phone: '',
-      DOB: '',
+  data() {
+    return {
+      model: {
+        name: '',
+        email: '',
+        phone: '',
+        date_of_birth: '',
+        experience_level: '',
+        already_participated: false,
+        character_id: 0,
+      },
+      level: "Personal info",
+      experiences: ['Beginner', 'Intermediate', 'Professional'],
+      characters: [],
     }
   },
-  methods:{
-    submit(){
+  mounted() {
+    let data = localStorage.getItem('user')
+    data = JSON.parse(data)
+    if(data){
+      this.model.name = data.name
+      this.model.email = data.email
+      this.model.phone = data.phone
+      this.model.date_of_birth = data.date_of_birth
+      this.model.character_id = data.character_id
+      this.model.already_participated = data.already_participated
 
+    }
+    fetch("https://chess-tournament-api.devtest.ge/api/grandmasters")
+        .then(res => res.json())
+        .then(data => this.characters = data)
+  },
+  methods: {
+    update(){
+      setTimeout(()=>{
+        localStorage.setItem("user",JSON.stringify(this.model))
+
+      },0)
     },
-    cancel(){
+    submit() {
+      this.level = 'Experience'
+    },
+    cancel() {
       this.$router.push({name: 'index'})
+    },
+    backLevel(){
+      this.level = "Personal info"
+    },
+    done() {
+        localStorage.removeItem("user")
     }
   }
 }
 </script>
 
 <style scoped>
-.main-container{
-  color: white;
+.main-container {
+  color: black;
 }
-.caption{
+
+.caption {
   color: black;
   max-width: 300px;
   position: absolute;
