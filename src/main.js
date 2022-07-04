5d2ef43125966a1d1d321443943a1c78eafdd027
leomw <bzishvili57@gmail.com>
2022-07-04 13:32:39 +0400

    import some fontAwesome icons

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {library} from "@fortawesome/fontawesome-svg-core";
 import {
     faCircleRight,
 }
-    from "@fortawesome/free-regular-svg-icons";
+    from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
 library.add(
     faCircleRight,
