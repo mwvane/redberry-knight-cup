852ba42826d47fddff40cda5f85c938636d24e68
leomw <bzishvili57@gmail.com>
2022-07-06 00:44:48 +0400

    make some changes for bug fixes

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,5 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: white;
-}
+  }
 </style>
---
+++ src/components/ProgressLevel.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="main-container d-flex">
+    <div v-for="level in levels" :key="level.number">
+      <div>
+        <div class="numeration">
+          <div v-if="level.status === active" class="active">
+            {{level.number}}
+          </div>
+          <div v-if="level.status === disabled" class="disabled">
+            {{level.number}}
+          </div>
+          <div v-if="level.status === done" class="done">
+            <font-awesome-icon icon="check-double"></font-awesome-icon>
+          </div>
+        </div>
+        <label style="font-size: 13px">{{level.name}}</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {status} from "@/constants";
+export default {
+  name: "ProgressLevel",
+  props:{
+    levels: Array
+  },
+  computed:{
+    active(){
+      return status.active
+    },
+    disabled(){
+      return status.disabled
+    },
+    done(){
+      return status.done
+    }
+  }
+}
+</script>
+
+<style scoped>
+.numeration{
+  width: 30px;
+  height: 30px;
+  border-radius: 6px;
+  border: 1px solid rgba(100,100,100, .3);
+  color: black;
+  font-size: 16px;
+  text-align: center;
+  margin-right: 100px;
+}
+.active{
+  font-weight: bold;
+  width: 100%;
+  height: 100%;
+  background-color: #E9FAF1;
+}
+.done{
+  width: 100%;
+  height: 100%;
+  background-color: #E9FAF1;
+  color: #3BAF77
+}
+.disabled{
+  width: 100%;
+  height: 100%;
+  background-color: #E5E6E8;
+}
+.line{
+  width: 100%;
+  height: 4px;
+  background-color: black;
+  position: absolute;
+  z-index: -1;
+  top: 12px
+}
+</style>
