7a2ea6ad3d0e5cd1f91c782e7c04f3cabe395ec6
leomw <bzishvili57@gmail.com>
2022-07-06 00:36:09 +0400

    Create CustomHeader.vue component

---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-container" :style="style">
+    <font-awesome-icon :icon="icon" class="px-1"></font-awesome-icon>
+    <label class="h-100 text py-1">{{text}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomHeader",
+  props:{
+    icon: {
+      default: "chess-queen"
+    },
+    text: {
+      default: "Redberry knight cup"
+    },
+    style: {
+      default: 'background: #7025FB'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-container{
+  height: 40px;
+  color: white;
+  padding-left: 20px;
+}
+</style>
