38a484323945cbb529a56796f629690b0768ebe8
leomw <bzishvili57@gmail.com>
2022-07-06 00:41:25 +0400

    remove img tag and use background-image instead

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="main-container">
+  <div class="main-container overflow-hidden">
     <div class="row">
-      <div class="col-6">
-        <div class="w-100 p-2" style="background-color: #7025FB">
-          <p>Redberry knight cup</p>
-        </div>
-        <img src="../assets/chess1.png" alt="no image" class="w-100">
+      <div class="col-6 image" :style="backgroundImage">
+        <CustomHeader class="w-100"></CustomHeader>
       </div>
-      <div class="col-6" style="background: #FD5334;">
-        <h1> CHES SAYS WHO WE ARE <span style="color: black; font-size: 16px">a lot about</span></h1>
-        <b-button @click="register">Get started <font-awesome-icon icon="circle-right"></font-awesome-icon></b-button>
+      <div class="col-6 d-flex justify-content-center align-items-center" style="background: #FD5334;">
+        <div class="w-50">
+          <h1> CHES SAYS <span style="color: black; font-size: 16px">a lot about</span> WHO WE ARE</h1>
+          <b-button @click="register" class="my-4">Get started <font-awesome-icon icon="circle-right"></font-awesome-icon></b-button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import CustomHeader from "@/components/CustomHeader";
 export default {
   name: "Index",
+  components:{CustomHeader},
+  computed:{
+    backgroundImage() {
+      return {
+        backgroundImage: `url(${require('../assets/chess1.png')})`,
+      }
+    }
+  },
   methods:{
     register(){
       this.$router.push({name: 'register'})
@@ -29,5 +37,9 @@ export default {
 <style scoped>
 .main-container{
   color: white;
+}
+.image{
+  height: 100vh;
+  background-size: cover;
 }
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,102 +1,38 @@
 <template>
-  <div class="main-container">
-    <div class="row" v-if="level === 'Personal info'">
-      <div class="col-6">
-        <div class="w-100 p-2" style="background-color: #7025FB;">
-          <p class="my-2">chess</p>
-        </div>
-        <img src="../assets/chess2.png" alt="no image" class="w-100">
-        <div class="caption">
-          <p>"WHEN YOU SEE A GOOD MOVIE, LOOK FOR A BETTER ONE"</p>
-          <p STYLE="color: white">-EMANUEL LASKER</p>
-        </div>
-      </div>
-      <div class="col-6">
-        <div class="w-100 p-2" style="border-bottom: 1px solid rgba(100,100,100, .2)">
-          <p class="my-2" style="color: black">Start Creating Your Account</p>
-        </div>
-        <h3 class="my-5" style="color: black">Personal Information</h3>
-        <b-form class="w-75 my-2">
-          <b-form-input
-              type="text"
-              class="my-3"
-              v-model="model.name"
-              @input="update"
-          ></b-form-input>
-          <b-form-input
-              type="email"
-              class="my-3"
-              v-model="model.email"
-              @input="update"
-          ></b-form-input>
-          <b-form-input
-              type="number"
-              class="my-3"
-              v-model="model.phone"
-              @input="update"
-          ></b-form-input>
-          <b-form-input
-              type="date"
-              class="my-3"
-              v-model="model.date_of_birth"
-              @input="update"
-          ></b-form-input>
-          <div class="d-flex justify-content-between py-5">
-            <b-button @click="cancel" variant="outline-dark"> Cancel</b-button>
-            <b-button @click="submit" variant="primary">Next</b-button>
-          </div>
-        </b-form>
-      </div>
+  <div class="main-container overflow-hidden">
+    <div v-show="level === personalInfo">
+      <PersonalInfoComponent :data="model"
+                             :levels="levels"
+                             @next="next"
+                             @cancel="cancel"
+                             @update="update"
+      ></PersonalInfoComponent>
     </div>
+    <div v-show="level === experience">
+      <ExperienceComponent :levels="levels"
+                           :data="model"
+                           :characters="characters"
+                           @update="update"
+                           @back="back"
+                           @done="done">
 
-    <div class="row" v-if="level === 'Experience'">
-      <div class="col-6">
-        <div class="w-100 p-2" style="background-color: #7025FB;">
-          <p class="my-2">chess</p>
-        </div>
-        <img src="../assets/chess3.png" alt="no image" class="w-100">
-        <div class="caption">
-          <p>"MANY HAVE BECOME CHESS MASTER; NO ONE HAS BECOME THE MASTER OF CHESS"</p>
-          <p STYLE="color: white">-SIEGBERT TARRASCH</p>
-        </div>
-      </div>
-      <div class="col-6">
-        <div class="w-100 p-2" style="border-bottom: 1px solid rgba(100,100,100, .2)">
-          <p class="my-2" style="color: black">First Step Is Done, Continue To Finish Onboarding</p>
-        </div>
-        <h3 class="my-5" style="color: black">Chess experience</h3>
-        <b-form class="w-75 my-2">
-          <b-form-select class="w-50" v-model="model.experience_level" @input="update">
-            <b-form-select-option v-for="item in experiences" :key="item" :value="item">
-              {{ item }}
-            </b-form-select-option>
-          </b-form-select>
-          <b-form-select class="w-50" v-model="model.character_id" @input="update">
-            <b-form-select-option v-for="person in characters" :key="person.id" :value="person.id">
-              {{ person.name }}
-            </b-form-select-option>
-
-          </b-form-select>
-          <div class="my-5">
-            <p>Have you participated in the Redberry Championship?</p>
-            <b-form-radio name="participated" v-model="model.already_participated" value="true">Yes</b-form-radio>
-            <b-form-radio name="participated" v-model="model.already_participated" value="false">No</b-form-radio>
-          </div>
-
-          <div class="d-flex justify-content-between py-5">
-            <b-button @click="backLevel" variant="outline-dark"> Back</b-button>
-            <b-button @click="done" variant="primary">Done</b-button>
-          </div>
-        </b-form>
-      </div>
+      </ExperienceComponent>
     </div>
 
   </div>
 </template>
 
 <script>
+import {level, status} from "@/constants";
+import PersonalInfoComponent from "@/components/PersonalInfoComponent";
+import ExperienceComponent from "@/components/ExperienceComponent";
+
 export default {
   name: "Register",
+  components: {
+    PersonalInfoComponent,
+    ExperienceComponent
+  },
   data() {
     return {
       model: {
@@ -109,61 +45,81 @@ export default {
         character_id: 0,
       },
       level: "Personal info",
-      experiences: ['Beginner', 'Intermediate', 'Professional'],
       characters: [],
+      currentLevelIndex: 0,
+      levels: [
+        {
+          name: "Personal info",
+          number: 1,
+          status: status.active
+        },
+        {
+          name: "Experience",
+          number: 2,
+          status: status.disabled
+        },
+      ]
     }
   },
   mounted() {
     let data = localStorage.getItem('user')
     data = JSON.parse(data)
-    if(data){
-      this.model.name = data.name
-      this.model.email = data.email
-      this.model.phone = data.phone
-      this.model.date_of_birth = data.date_of_birth
-      this.model.character_id = data.character_id
-      this.model.already_participated = data.already_participated
-
+    if (data) {
+      for (const key in data) {
+        if (Object.prototype.hasOwnProperty.call(this.model, key)) {
+          this.model[key] = data[key]
+        }
+      }
     }
     fetch("https://chess-tournament-api.devtest.ge/api/grandmasters")
         .then(res => res.json())
         .then(data => this.characters = data)
   },
-  methods: {
-    update(){
-      setTimeout(()=>{
-        localStorage.setItem("user",JSON.stringify(this.model))
-
-      },0)
+  computed: {
+    personalInfo() {
+      return level.personalInfo
     },
-    submit() {
-      this.level = 'Experience'
+    experience() {
+      return level.experience
+    },
+  },
+  methods: {
+    update() {
+      setTimeout(() => {
+        localStorage.setItem("user", JSON.stringify(this.model))
+      }, 0)
+    },
+    next() {
+      this.level = level.experience
+      debugger
+      this.levels[this.currentLevelIndex].status = 'done'
+      this.levels[this.currentLevelIndex + 1].status = 'active'
+      this.level = this.levels[this.currentLevelIndex].name
     },
     cancel() {
       this.$router.push({name: 'index'})
     },
-    backLevel(){
-      this.level = "Personal info"
+    back() {
+      this.level = level.personalInfo
+      this.levels[this.currentLevelIndex].status = status.disabled
+      this.levels[--this.currentLevelIndex].status = status.active
     },
     done() {
-        localStorage.removeItem("user")
+      localStorage.removeItem("user")
+      this.levels[this.currentLevelIndex].status = status.done
+      this.$router.push({name: 'completed'})
     }
   }
 }
 </script>
 
 <style scoped>
-.main-container {
-  color: black;
-}
 
 .caption {
   color: black;
   max-width: 300px;
-  position: absolute;
   top: 211px;
   left: 132px;
   font-weight: bold;
-
 }
 </style>
